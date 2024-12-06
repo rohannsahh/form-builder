1b354266d2a913f6a24c8ce0543175646824f653
Rohan <rohannsahh@gmail.com>
2024-12-07 00:04:02 +0530

    latest

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  esbuild: {
+    // Disable TypeScript type checking
+    tsconfig: false,
+  },
   plugins: [react()],
 })
